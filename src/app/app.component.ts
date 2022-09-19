212c3a9c12843fa900cc81b1b1618344d8881310
Thiago Gorza <Thiago Gorza>
2022-09-19 15:02:42 -0300

    Modificações feitas no template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basico-transferencias';
-  transferencia: any;
+  transferencias:any[] = [];
 
   transferir($event:any) {
-    console.log($event)
-   this.transferencia = $event;
+   const transferencia = {...$event,data: new Date()}
+   this.transferencias.push(transferencia);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import {FormsModule} from "@angular/forms";
 import { ExtratoComponent } from './extrato/extrato.component';
+import ptBr from '@angular/common/locales/pt';
+import {registerLocaleData} from "@angular/common";
+registerLocaleData(ptBr)
 
 @NgModule({
   declarations: [
@@ -16,7 +19,7 @@ import { ExtratoComponent } from './extrato/extrato.component';
         BrowserModule,
         FormsModule
     ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
